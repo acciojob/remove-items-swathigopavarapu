f7dba37b0cb72e3a4e3ffe61104b9c9a33f172ef
swathigopavarapu <41499776+swathigopavarapu@users.noreply.github.com>
2025-08-29 10:56:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+window.onload = function () {
+  const button = document.querySelector('input[type="button"]');
+  const select = document.getElementById('colorSelect');
+
+  button.addEventListener('click', function () {
+    // Get selected index
+    const selectedIndex = select.selectedIndex;
+
+    // If a valid option is selected, remove it
+    if (selectedIndex !== -1) {
+      select.remove(selectedIndex);
+    }
+  });
+};
